1e79e17a469d30c5ee87c8ad7988e4a3a67703c4
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-01-16 15:15:23 -0500

    ChatApp
    A react chat app that allows you to send messages over a UI that is powered by firebase.

---
+++ src/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Avatar, IconButton } from '@mui/material'
+import ChatIcon from '@mui/icons-material/Chat';
+import { DonutLarge } from '@mui/icons-material'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder='Search or start new chat' />
+        </div>
+      </div>
+      <div className="sidebar__chats"></div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYVfY5w-3BSTHAn9dWH0D0x24gzNZQE5c",
+  authDomain: "chat-app-3de05.firebaseapp.com",
+  projectId: "chat-app-3de05",
+  storageBucket: "chat-app-3de05.appspot.com",
+  messagingSenderId: "112807340351",
+  appId: "1:112807340351:web:c4e3331ee2851af9278b8a",
+  measurementId: "G-PDJKCF2EYS"
+};
