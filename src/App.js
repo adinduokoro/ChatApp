f4ae9461f97f9d917c933ca816f28c1ea3d1a0e6
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-01-17 15:40:14 -0500

    Working on the Login using Google Auth.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat.js'
 import { Routes , Route } from 'react-router';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <div className="app">
-      <div className="app__body">
-        <Sidebar />
-          <Routes>
-            <Route index path="/" element={<Chat/>} />
-            <Route path="/rooms/:roomId" element={<Chat />} />
-          </Routes>
-      </div>
+
+      {!user ? ( 
+        <h1>Login</h1>
+      ) : ( 
+        <div className="app__body">
+          <Sidebar />
+            <Routes>
+              <Route index path="/" element={<Chat/>} />
+              <Route path="/rooms/:roomId" element={<Chat />} />
+            </Routes>
+        </div>
+      )}
     </div>
   );
 }
