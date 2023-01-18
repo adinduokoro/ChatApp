3d45c55641619cd578ce44b495fd04047ce765f4
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-01-17 20:01:59 -0500

    Potential Router Fix

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat.js'
-import { Routes , Route } from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
+
   return (
     <div className="app">
       <div className="app__body">
-        <Sidebar />
-          <Routes>
-            <Route index path="/" element={<Chat/>} />
-            <Route path="/rooms/:roomId" element={<Chat />} />
-          </Routes>
+        <Router>
+          <Sidebar />
+            <Routes>
+              <Route element={<Chat />} />
+              <Route path="rooms">
+                <Route path=":roomId" element={<Chat />} />
+              </Route>
+            </Routes>
+        </Router>
       </div>
     </div>
+
+  // <div className="app">
+  // <div className="app__body">
+  //   <Router>
+  //     <Sidebar />
+  //       <Routes>
+  //         <Route index element={<Chat />} />
+  //         <Route path="/rooms/:roomId" element={<Chat />} />
+  //       </Routes>
+  //   </Router>
+  // </div>
+  // </div>
   );
 }
 
